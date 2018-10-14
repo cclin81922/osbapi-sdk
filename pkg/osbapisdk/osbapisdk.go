55b9092eb2ef32ab176e4b70b2a6f82e802515ed
cclin <jimlintw922@gmail.com>
2018-10-14 17:43:04 +0800

    add pki

--- pkg/osbapisdk/osbapisdk.go
+++ pkg/osbapisdk/osbapisdk.go
@@ -1,12 +1,82 @@
 package osbapisdk
 
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var (
+	baseurlFile string
+	caFile      string
+	keyFile     string
+	certFile    string
+)
+
+func init() {
+	baseurlFile = "/etc/osbapi-svc-credentials/baseurl"
+	caFile = "/etc/osbapi-svc-credentials/ca"
+	keyFile = "/etc/osbapi-svc-credentials/key"
+	certFile = "/etc/osbapi-svc-credentials/cert"
+}
+
+// Setup ...
+func Setup(baseurl, ca, key, cert string) {
+	baseurlFile = baseurl
+	caFile = ca
+	keyFile = key
+	certFile = cert
+}
+
 // Echo ...
 func Echo(message string) (string, error) {
-	// TODO
+	baseurlBytes, err := ioutil.ReadFile(baseurlFile)
+	if err != nil {
+		return "", err
+	}
+	baseurl := string(baseurlBytes)
+	api := fmt.Sprintf("%s/echo", baseurl)
 
-	// NOTE
-	// file /etc/osbapi-svc-credentials/param-1 content value-1
-	// file /etc/osbapi-svc-credentials/param-2 content value-2
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return "", err
+	}
 
-	return message, nil
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: true,
+	}
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+
+	// Send HTTP request
+	resp, err := client.Post(api, "", bytes.NewBufferString(message))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	replyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	reply := string(replyBytes)
+
+	return reply, nil
 }
