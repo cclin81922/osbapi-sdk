e492caefc3afbaa4e88c6953365073b4bb5cb055
cclin <jimlintw922@gmail.com>
2018-10-14 13:47:08 +0800

    add comment

--- pkg/osbapisdk/osbapisdk.go
+++ pkg/osbapisdk/osbapisdk.go
@@ -1,5 +1,12 @@
 package osbapisdk
 
+// Echo ...
 func Echo(message string) (string, error) {
-    return message, nil
+	// TODO
+
+	// NOTE
+	// file /etc/osbapi-svc-credentials/param-1 content value-1
+	// file /etc/osbapi-svc-credentials/param-2 content value-2
+
+	return message, nil
 }
